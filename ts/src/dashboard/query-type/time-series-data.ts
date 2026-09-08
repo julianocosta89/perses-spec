90a9876f232d2d2e8adc0e2a5961f26cc8075067
Juliano Costa <julianocosta89@outlook.com>
2026-09-08 07:58:48 +0000

    Exemplars/3445 shared contracts (#97)
    * feat: add exemplars to TimeSeriesData contract
    
    Extend the time-series query data model with an optional `exemplars`
    field mirroring the Prometheus query_exemplars response shape
    (TimeSeriesExemplars: { seriesLabels, exemplars: [{ labels, value,
    timestamp }] }).
    
    The field is optional so existing query plugins remain backward
    compatible. Timestamps are UnixTimeMs (ms) and values are numbers;
    datasource plugins are responsible for converting raw Prometheus
    responses (string values, second-based timestamps).
    
    Related to perses/perses#3445
    
    Signed-off-by: Juliano Costa <juliano.costa@datadoghq.com>
    
    * Remove unnecessary comments
    
    Signed-off-by: Juliano Costa <juliano.costa@datadoghq.com>
    
    ---------
    
    Signed-off-by: Juliano Costa <juliano.costa@datadoghq.com>

--- ts/src/dashboard/query-type/time-series-data.ts
+++ ts/src/dashboard/query-type/time-series-data.ts
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import type { AbsoluteTimeRange } from '../../common';
+import type { AbsoluteTimeRange, UnixTimeMs } from '../../common';
 import type { BaseMetadata } from './base-metadata';
 import type { Labels, TimeSeriesValueTuple, TimeSeriesHistogramTuple } from './time-series-queries';
 
@@ -26,6 +26,7 @@ export interface TimeSeriesData {
   timeRange?: AbsoluteTimeRange;
   stepMs?: number;
   series: TimeSeries[];
+  exemplars?: TimeSeriesExemplars[];
   metadata?: TimeSeriesMetadata;
 }
 
@@ -39,4 +40,15 @@ export interface TimeSeries {
 
 export interface TimeSeriesMetadata extends BaseMetadata {
   [key: string]: unknown;
+}
+
+export interface Exemplar {
+  labels: Labels;
+  value: number;
+  timestamp: UnixTimeMs;
+}
+
+export interface TimeSeriesExemplars {
+  seriesLabels: Labels;
+  exemplars: Exemplar[];
 }
